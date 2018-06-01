e05af17b208ea2e1dbc81e0b1c1fb046afd09005
Lauren Kaapcke <lkaapcke@ucsb.edu>
2018-05-31 23:05:15 -0700

    First attempt at function

---
+++ fishery.R
@@ -0,0 +1,11 @@
+# Fishery summary
+#
+# Return the most frequently caught fish
+# Sum total revenue for each location
+# Sum total fishery revenue
+# Graph revenue by location with total revenue label if user requests
+
+fishery_summary = function (price, landing) {
+  most_frequent = max (landing)
+  return(most_frequent)
+}
